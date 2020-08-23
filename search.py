2d5ec833e169b0d23197277ca8ea8892df6cc7e8
Ofek Gila <ofek@theofekfoundation.org>
2020-08-23 13:15:35 -0700

    Massive performance improvements (asymptotically faster, e.g. running until row 2001 the new way is around 15 times faster than running until row 401 the old way.
    Also a bit cleaner / more robust code
    
    Also timing was added

--- search.py
+++ search.py
@@ -1,36 +1,49 @@
-#Use this code to find which numbers repeat an interesting number of times in Pascal's triangle.
+# Use this code to find which numbers repeat an interesting number of times in Pascal's triangle.
 
-import math 
+import time
+from collections import defaultdict
 
-def search(row_limit):
-    list_numbers = []
-    
-    #Below I am dictating the rows and columns I will compute cell values for. 
-    
-    #(I have excluded the outer two diagonals (on either side) of Pascal's triangle, because we know that the numbers in those diagonals (across the whole triangle) are an infinite number of 1s and two appearances of all other positive integers apart from 2 (for which there is only one appearance).)
-    
-    for n in range(4,row_limit):
-        for r in range(2,n-1):
-            nCr = math.factorial(n)/(math.factorial(n-r)*math.factorial(r))
-            list_numbers.append(nCr)
-    
-    set_numbers = set(list_numbers)
-    how_many_times = []
-    which_ones = []
-    
-    #Below I am checking how many times each distinct number appears in the cells we've generated. If a number appears more than twice (therefore more than four times across the whole triangle) I am storing both the number and the minimum number of times it appears across the whole triangle.
-    
-    for number in set_numbers:
-        count = list_numbers.count(number)
-        if count >= 3:
-            how_many_times.append(count+2)
-            which_ones.append(number)
-        
-    final_info = dict(zip(which_ones,how_many_times))
-    
-    for entry,frequency in final_info.items(): 
-        print ("The number {0} appears at least {1} times across the whole triangle".format(entry,frequency))
-        
-    return "END"
+def search(row_limit: int, minimum_counts: int = 5) -> [(int, int)]:
+    # A dictionary representing how many times a number appears in the cells we've generated. Every number appears once in the outer diagonals,
+    # so we can begin the counts at 2
+    counts = defaultdict(lambda: 2)
 
-search(501)
+    # Below I am dictating the rows and columns I will compute cell values for.
+
+    # (I have excluded the outer two diagonals (on either side) of Pascal's triangle, because we know that the numbers in those diagonals (across the whole triangle) are an infinite number of 1s and two appearances of all other positive integers apart from 2 (for which there is only one appearance).)
+
+    # Faster method for calculating subsequent nCr for performance reasons
+    # While this part was by no means the bottleneck, it's still nice to have
+    for n in range(4, row_limit):
+        nCr = n
+        for r in range(2, n - 1):
+            nCr *= n - r + 1
+            nCr //= r
+            counts[nCr] += 1
+
+    # Below I am checking how many times each distinct number appears in the cells we've generated. If a number appears more than four times, print it.
+
+    matches = list()
+
+    for entry, frequency in counts.items():
+        if frequency < minimum_counts:
+            continue
+
+        matches.append((entry, frequency))
+
+    return matches
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    matches = search(3001, 5)
+
+    for entry, frequency in matches:
+        print(f"The number {entry:,} appears at least {frequency} times across the whole triangle")
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f'Ran in {elapsed_time} seconds')
+
